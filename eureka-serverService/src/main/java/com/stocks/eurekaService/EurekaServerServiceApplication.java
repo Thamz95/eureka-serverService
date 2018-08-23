0bcd407815a5120981862f9db7e5e7cba77ac209
Thamaraiselvan <thamurug@in.ibm.com>
2018-08-23 15:53:44 +0530

    first project

---
+++ eureka-serverService/src/main/java/com/stocks/eurekaService/EurekaServerServiceApplication.java
@@ -0,0 +1,16 @@
+package com.stocks.eurekaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerServiceApplication.class, args);
+	}
+}
